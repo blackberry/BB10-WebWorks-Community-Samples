d37f3f70abaf6895246f140671aaf03d5bd8cddc
dmcwong <dmcwong@users.noreply.github.com>
2015-04-22 10:52:26 -0600

    Update index.js

--- pdbtest/www/js/index.js
+++ pdbtest/www/js/index.js
@@ -25,7 +25,7 @@ var app = {
         remotedb: null,
         db: null,
         rep: null,
-        remotedbName: 'http://192.168.1.142:4984/sync_gateway' // this must be set to point to your gateway
+        remotedbName: 'YOUR URL HERE' // this must be set to point to your gateway
     },
 
     offline: true,
